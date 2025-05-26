ca801ce88ce0e0a51765ecab37087bd9f5875e87
EA - Maria Vitória Barreto Queiroz <queiroz_vitoria@educadventista.org>
2025-05-26 15:11:48 -0300

    segundo commit

---
+++ components/Exemplo1.jsx
@@ -0,0 +1,7 @@
+export function Exemplo1() {
+    return (
+     <section>
+        <h2> Exemplo 1: utilizações do use Effect</h2>
+     </section>
+    )
+}
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return (
+        <header>
+            <h1>UseEffect no React </h1>
+        </header>
+
+
+    )
+}
